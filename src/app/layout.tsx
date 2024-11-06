4c6783092831c6b73a5ba709650f52ab05ea84b3
Sahil Harriram <sahil.harriram@gmail.com>
2024-11-06 15:25:49 +0700

    fixed poppins variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import "../styles/globals.css";
-
 import CssBaseline from "@mui/material/CssBaseline";
 import type { Session } from "next-auth";
 import { SoundProvider } from "@/context/SoundContext";
-import { LoadingProvider, useLoading } from "@/context/LoadingContext";
+import { LoadingProvider } from "@/context/LoadingContext";
 import GlobalScrollbarStyles from "@/theme/TextStyles/ScrollBar/scrollBarStyles";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import WalletConfiguration from "@/hooks/WalletConfiguration";
@@ -18,7 +17,7 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
 });
 
-// Load custom "Vera Humana 95" font from src/fonts/
+// Load custom "Vera Humana 95" font
 const veraHumana = localFont({
   src: [
     {
@@ -42,7 +41,7 @@ export default function RootLayout({
   session,
 }: Readonly<{ session: Session; children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${veraHumana.variable} ${poppins.variable}`}>
+    <html lang="en" className={`${veraHumana.variable} ${poppins}`}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -67,7 +66,7 @@ export default function RootLayout({
         <link rel="canonical" href="https://buzzkill.world/" />
         <title>Buzzkill - Play Game</title>
       </head>
-      <body>
+      <body className={veraHumana.className}>
         <WalletConfiguration session={session}>
           <LoadingProvider>
             <SoundProvider>
@@ -76,7 +75,6 @@ export default function RootLayout({
                 <GlobalScrollbarStyles />
                 <EnvironmentProvider>
                   {children}
-
                   <SpeedInsights />
                 </EnvironmentProvider>
               </UserProvider>
