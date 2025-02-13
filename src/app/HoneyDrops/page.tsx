27af067e60a2f02aea5f7f0b95ff87cd8e3ae779
Sahil Harriram <sahil.harriram@gmail.com>
2025-02-13 15:06:55 +0700

    fixed reward rate on invites

--- src/app/HoneyDrops/page.tsx
+++ src/app/HoneyDrops/page.tsx
@@ -65,7 +65,7 @@ const HoneyDropsPage = () => {
           // Handle constants data
           setConstants({
             dailyBonus: constantsResponse.dailyBonus ?? 100,
-            referralReward: constantsResponse.referralReward ?? 1000,
+            referralReward: constantsResponse.referralReward ?? 500,
           });
 
           setError(null); // Clear any previous errors
@@ -76,7 +76,7 @@ const HoneyDropsPage = () => {
           setError(err.message || "Failed to fetch leaderboard data.");
           setConstants({
             dailyBonus: 100,
-            referralReward:1000,
+            referralReward: 500,
           });
         }
       } finally {
